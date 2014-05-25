2c398559aceba954c76ff1c26b06da3ccaf42f74
deadprogram <ron@hybridgroup.com>
2014-05-25 06:57:34 -0700

    Pins A4 and A5 also support PWM just fine

--- src/voodoospark.cpp
+++ src/voodoospark.cpp
@@ -50,11 +50,11 @@ long SerialSpeed[] = {
 /*
   PWM/Servo support is CONFIRMED available on:
 
-  D0, D1, A0, A1, A5, A7
+  D0, D1, A0, A1, A4, A5, A7
 
   Allocate 6 servo objects:
  */
-Servo servos[6];
+Servo servos[7];
 /*
   The Spark board can only support PWM/Servo on specific pins, so
   based on the pin number, determine the servo index for the allocated
@@ -65,10 +65,12 @@ int ToServoIndex(int pin) {
   if (pin == 0 || pin == 1) return pin;
   // A0, A1
   if (pin == 10 || pin == 11) return pin - 8;
-  // A5
+  // A4
   if (pin == 15) return 4;
+  // A5
+  if (pin == 16) return 5;
   // A7
-  if (pin == 17) return 5;
+  if (pin == 17) return 6;
 }
 
 
